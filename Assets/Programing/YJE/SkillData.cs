f5bb0276d3e485ecfde0cf3da9c1123b61203362
YIJEUN <yijeun.dev@gmail.com>
2024-12-16 17:46:58 +0900

    [feat] Data Scriptable Object
    각 Data의 Scriptable Object Test

---
+++ Assets/Programing/YJE/SkillData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillData : ScriptableObject
+{
+    // TODO : 스킬관련 데이터 변수 설정
+    public virtual void DoSkill() { }
+}
---
+++ Assets/Programing/YJE/WarriorSkill.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WarriorSkill")]
+public class WarriorSkill : SkillData
+{
+    public override void DoSkill()
+    {
+        // TODO : 전사 스킬의 사용 내용 
+    }
+}
