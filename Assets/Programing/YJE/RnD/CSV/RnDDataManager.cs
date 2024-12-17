327000f0bd3af8bc937b631be5efa3e02c7e4d0f
YIJEUN <yijeun.dev@gmail.com>
2024-12-17 18:25:15 +0900

    [feat] RnD CSV
    CSV로 받아오는 Data의 저장 RnD

---
+++ Assets/Programing/YJE/RnD/CSV/RnDDataManager.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+// 알아두기
+// inspector 창에서 넣는 _urls의 형식은 마지막 /edit~ 부분을 /export?=format=csv 형식으로 변환하여 사용
+// ex)
+// https://docs.google.com/spreadsheets/d/1KXfqH_i8RFvsEWVfqmSbnVuuLuMm6GvW3kF6nt5iMI4/edit?gid=0#gid=0 이 url을
+// https://docs.google.com/spreadsheets/d/1KXfqH_i8RFvsEWVfqmSbnVuuLuMm6GvW3kF6nt5iMI4/export?=format=csv 로 변경
+
+public class RnDDataManager : MonoBehaviour
+{
+    private static RnDDataManager _instance;
+    public static RnDDataManager Instance { get { return _instance; } set { _instance = value; } }
+
+    //csv 링크들
+    [SerializeField] private string[] _urls;
+
+    //csvData들
+    [SerializeField] private string[] _csvDatas;
+
+    //csvData Parsing한 Data들
+    public List<Dictionary<string, string>>[] DataLists { get; set; }
+
+    UnityWebRequest _request;
+
+    private Coroutine _downLoadRoutine;
+
+    private bool _isLoad;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            if (_isLoad == false)
+            {
+                _downLoadRoutine = StartCoroutine(DownloadRoutine());
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator DownloadRoutine()
+    {
+        _isLoad = true;
+
+        // Data 초기화 
+        DataLists = new List<Dictionary<string, string>>[_csvDatas.Length];
+        for (int i = 0; i < DataLists.Length; i++)
+        {
+            DataLists[i] = new List<Dictionary<string, string>>();
+        }
+
+        for (int i = 0; i < _urls.Length; i++)
+        {
+            _request = UnityWebRequest.Get(_urls[i]);
+
+            // 요청 후 파일다운로드 완료까지 대기
+            yield return _request.SendWebRequest();
+
+            //다운로드 완료 후 string에 저장.
+            _csvDatas[i] = _request.downloadHandler.text;
+
+            //Parsing에서 List에 저장.
+            DataLists[i] = ChangeCsvToList(_csvDatas[i]);
+        }
+    }
+
+    List<Dictionary<string, string>> ChangeCsvToList(string data)
+    {
+        List<Dictionary<string, string>> dataList = new List<Dictionary<string, string>>();
+
+        string[] lines = data.Split('\n');
+
+        Debug.Log(lines.Length);
+
+        // CSV 첫 줄은 헤더
+        string[] headers = lines[0].Split(',');
+
+        // CSV 데이터 파싱
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] values = lines[i].Split(',');
+            Dictionary<string, string> dataDic = new Dictionary<string, string>();
+
+            for (int j = 0; j < headers.Length; j++)
+            {
+                dataDic[headers[j]] = values[j];
+            }
+
+            dataList.Add(dataDic);
+        }
+
+        //foreach (var expando in dataList)
+        //{
+        //    foreach (var pair in expando)
+        //    {
+        //        Debug.Log($"{pair.Key}: {pair.Value}");
+        //    }
+        //}
+
+        return dataList;
+    }
+
+
+}
---
+++ Assets/Programing/YJE/RnD/CSV/RnDStageParser.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RnDStageParser : MonoBehaviour
+{
+    // DataManager에서 가져올 DataLists를 받을 List제작
+    List<Dictionary<string, string>> dictionary = new List<Dictionary<string, string>>();
+
+    // 원하는 데이터를 형전환 후 저장할 리스트(Stage 자료형)
+    List<RnDStage> stages = new List<RnDStage>();
+    private void Update()
+    {
+        // 자료 테스트 용
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DataLoadTest();
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            DataShow();
+        }
+    }
+
+    private void DataLoadTest()
+    {
+        // DataManager.cs에서 파싱된 데이터 불러오기
+        dictionary = RnDDataManager.Instance.DataLists[0];
+        // [index][원하는 항목 제목 string]
+        for(int i = 0; i < dictionary.Count; i++)
+        {
+            Debug.Log(dictionary[i]["Id"]);
+            Debug.Log(dictionary[i]["StageName"]);
+            Debug.Log(dictionary[i]["TimeLimit"]);
+            Debug.Log(dictionary[i]["MonsterCount"]);
+            //Debug.Log(dictionary[i]["MonsterPos"]);
+
+            RnDStage stage = new RnDStage();
+            // 각 stage의 변수들의 자료형에 맞추어 변환 후 저장
+            stage.id = int.Parse(dictionary[i]["Id"]);
+            stage.stageName = dictionary[i]["StageName"];
+            stage.timeLimit = int.Parse(dictionary[i]["TimeLimit"]);
+            stage.monsterCount = int.Parse(dictionary[i]["MonsterCount"]);
+            //stage.monsterPos = int.Parse(dictionary[i]["MonsterPos"]);
+            stages.Add(stage); // 완성된 stage를 stages 리스트에 저장
+        }
+    }
+    private void DataShow()
+    {
+        for (int i = 0; i < stages.Count; i++)
+        {
+            Debug.Log($"{i} 번째");
+            Debug.Log(stages[i].id);
+            Debug.Log(stages[i].stageName);
+            Debug.Log(stages[i].timeLimit);
+            Debug.Log(stages[i].monsterCount);
+            //Debug.Log(stages[i].monsterPos);
+        }
+    }
+}
