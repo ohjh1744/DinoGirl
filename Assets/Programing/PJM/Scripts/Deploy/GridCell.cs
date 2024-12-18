497ca91184d1e88f5d83e7aaf5f5a0a540a406dd
LemoZun <marrzun@gmail.com>
2024-12-18 16:17:38 +0900

    feat : 그리드 시스템 및 버프 시스템 기초 틀 생성

---
+++ Assets/Programing/PJM/Scripts/Deploy/GridCell.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell
+{
+    private Vector2Int _position;
+    private UnitController _unit;
+    public Vector2Int Position { get => _position; private set => _position = value; }
+    private bool _isDeployed;
+    
+    public bool IsDeployed { get => _isDeployed; private set => _isDeployed = value; }
+
+    public GridCell(Vector2Int position)
+    {
+        Position = position;
+        IsDeployed = false;
+    }
+    
+    
+
+    public void ApplyBuff()
+    {
+        // Todo : 버프적용 로직
+        Debug.Log("버프 적용됨");
+    }
+}
