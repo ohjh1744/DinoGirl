e933a7bf695c40874e25665f15db496f7a5b6cc4
whs1997 <whs1997@naver.com>
2024-12-19 19:09:58 +0900

    [FEAT] UI 바인딩 테스트

--- Assets/Programing/WHS/Scripts/CharacterInventoryUI.cs
+++ Assets/Programing/WHS/Scripts/CharacterInventoryUI.cs
@@ -14,19 +14,19 @@ public class CharacterInventoryUI : MonoBehaviour
 
     private void Start()
     {
-        Sprite tyranoSprite = Resources.Load<Sprite>("1");
-        Sprite yranoSprite = Resources.Load<Sprite>("2");
-        Sprite ranoSprite = Resources.Load<Sprite>("3");
-        Sprite anoSprite = Resources.Load<Sprite>("4");
-        Sprite noSprite = Resources.Load<Sprite>("5");
-        Sprite oSprite = Resources.Load<Sprite>("6");
-
-        AddCharacter(new Character { Name = "tyrano", level = 10, image = tyranoSprite });
-        AddCharacter(new Character { Name = "yrano", level = 50, image = yranoSprite });
-        AddCharacter(new Character { Name = "rano", level = 33, image = ranoSprite });
-        AddCharacter(new Character { Name = "ano", level = 44, image = anoSprite });
-        AddCharacter(new Character { Name = "no", level = 12, image = noSprite });
-        AddCharacter(new Character { Name = "o", level = 21, image = oSprite });
+        // 캐릭터는 데이터베이스에서 받아와서 쓸것,  스크롤뷰 확인용
+        AddCharacter(new Character { Name = "tyrano", level = 10 });
+        AddCharacter(new Character { Name = "yrano", level = 50 });
+        AddCharacter(new Character { Name = "rano", level = 33 });
+        AddCharacter(new Character { Name = "ano", level = 44 });
+        AddCharacter(new Character { Name = "no", level = 12 });
+        AddCharacter(new Character { Name = "o", level = 21 });
+        AddCharacter(new Character { Name = "tyrano", level = 10 });
+        AddCharacter(new Character { Name = "yrano", level = 50 });
+        AddCharacter(new Character { Name = "rano", level = 33 });
+        AddCharacter(new Character { Name = "ano", level = 44 });
+        AddCharacter(new Character { Name = "no", level = 12 });
+        AddCharacter(new Character { Name = "o", level = 21 });
 
         PopulateGrid();
     }
@@ -40,13 +40,13 @@ public class CharacterInventoryUI : MonoBehaviour
     // 그리드에 가진 캐릭터 정렬
     public void PopulateGrid()
     {
-        foreach(Transform child in content)
+        foreach (Transform child in content)
         {
             Destroy(child.gameObject);
         }
 
         // 캐릭터 정렬
-        foreach(Character character in characterList)
+        foreach (Character character in characterList)
         {
             GameObject slot = Instantiate(characterPrefab, content);
             CharacterSlotUI slotUI = slot.GetComponent<CharacterSlotUI>();
