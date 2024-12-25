3830e43895d16aab0781a8353a237ecd28481895
LemoZun <marrzun@gmail.com>
2024-12-25 16:36:48 +0900

    feat : ui 바인더를 사용한 스킬 ui 구현중

--- Assets/Programing/PJM/Scripts/Skill/BattleSceneUIView.cs
+++ Assets/Programing/PJM/Scripts/Skill/BattleSceneUIView.cs
@@ -1,27 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class BattleSceneUIView : UIBInder
 {
-    [SerializeField] private Button autoButton;
-    [SerializeField] private TMP_Text autoText;
+    private Image[] _hideSkillButtonImages;
+    private TextMeshProUGUI[] _hideSkillTexts;
+    private Image[] _hideSkillImages;
+    private bool[] _isHideSkills;
+    private float[] _skillTimes;
+    private float[] _getSkillTimes;
+
+    public static event Action<int> OnSkillUsed;
+    
+    
+    private Button _autoButton;
+    private TMP_Text _autoText;
+    
+    
 
     public bool isAutoOn;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         BindAll();
-        autoButton = GetUI<Button>("AutoButton");
-        autoText = GetUI<TMP_Text>("AutoText");
-        autoButton.onClick.AddListener(ToggleAuto);
+        _hideSkillButtonImages[0] = GetUI<Image>("HideImage1");
+        _hideSkillButtonImages[1] = GetUI<Image>("HideImage2");
+        
+        _hideSkillTexts[0] = GetUI<TextMeshProUGUI>("CooldownText1");
+        _hideSkillTexts[1] = GetUI<TextMeshProUGUI>("CooldownText2");
+        
+        _hideSkillImages[0] = GetUI<Image>("HideImage1");
+        _hideSkillImages[1] = GetUI<Image>("HideImage2");
+        
+        AddEvent("HideImage1",EventType.Click, (PointerEventData data) => OnSkillButtonTouched(0));
+        AddEvent("HideImage1",EventType.Click, (PointerEventData data) => OnSkillButtonTouched(1));
+
     }
 
-    // Update is called once per frame
+    
+    void Start()
+    {
+        
+        _autoButton = GetUI<Button>("AutoButton");
+        _autoText = GetUI<TMP_Text>("AutoText");
+        _autoButton.onClick.AddListener(ToggleAuto);
+    }
+
     void Update()
+    {
+        
+    }
+    
+    
+    private void OnSkillButtonTouched(int skillIndex)
+    {
+        Debug.Log($"{skillIndex} 번째 스킬버튼 클릭됨");
+    }
+
+    // 스킬 인덱스 스킬 쿨타임 UI 활성화
+    public void HideSkillSetting(int skillIndex)
+    {
+        _hideSkillImages[skillIndex].gameObject.SetActive(true);
+        _getSkillTimes[skillIndex] = _skillTimes[skillIndex];
+        _isHideSkills[skillIndex] = true;
+    }
+
+    public void HideSkillCheck()
     {
         
     }
@@ -29,7 +79,7 @@ public class BattleSceneUIView : UIBInder
     public void ToggleAuto()
     {
         isAutoOn = !isAutoOn;
-        autoText.text = $"Auto : {isAutoOn}";
+        _autoText.text = $"Auto : {isAutoOn}";
         Debug.Log($" 현재 오토 상태 : {isAutoOn}");
     }
 }
