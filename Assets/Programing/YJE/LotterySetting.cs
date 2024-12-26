2bd60c5706d72afe7aa20e7f0476e395c12cd39d
YIJEUN <yijeun.dev@gmail.com>
2024-12-26 21:31:31 +0900

    [feat] LotterySystem Tester 구현
    - LotterySystem의 기본 뽑기 시스템 구현
    - 정해진 데이터에서 가중치로 뽑기 구현
    - 확률 Debug로 확인 완료

--- Assets/Programing/YJE/LotterySetting.cs
+++ Assets/Programing/YJE/LotterySetting.cs
@@ -1,11 +1,20 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// DBTest와 연동하여 DataManager가 Data를 전부 불러온 후에 각종 세팅을 진행
+/// - 전체적인 씬의 진행사항에 필요한 정보들과 다른 클래스나 데이터와 연동된 정보를 저장
+/// - 씬이 로딩중일 때 모든 세팅을 마친 후 플레이어가 뽑기를 실행 할 수 있어야함
+/// - 전체 설정 후 LoadingPanel 비활성화 + button 활성화 하면서 뽑기를 실행
+/// - 로비씬과 연동시 DBTest가 사라지니 LoadingPanel이 활성화 된 상태에서 세팅을 진행하고
+///   세팅이 완료되면 전체 설정 후 LoadingPanel 비활성화 + button 활성화 하면서 뽑기를 실행하도록 해야함
+/// </summary>
 public class LotterySetting : MonoBehaviour
 {
-    // csvDataManager.cs에서 가져올 특정 DataList를 받을 List
-    List<Dictionary<string, string>> settingList = new List<Dictionary<string, string>>();
+    // csvDataManager.cs에서 가져올 특정 DataList를 받을 Disctionary
+    [SerializeField] Dictionary<int, Dictionary<string, string>> gachaList = new Dictionary<int, Dictionary<string, string>>();
+
+    //List<Dictionary<string, string>> settingList = new List<Dictionary<string, string>>();
 
     [Header("UI")]
     [SerializeField] private GameObject singleBtn;
@@ -14,6 +23,7 @@ public class LotterySetting : MonoBehaviour
 
     private void Awake()
     {
+
         // TODO :
         // MakeLotteryList() 실행
         // LotteryDataLoad() 실행
@@ -36,6 +46,12 @@ public class LotterySetting : MonoBehaviour
 
     private void MakeLotteryList()
     {
+        gachaList = CsvDataManager.Instance.DataLists[(int)E_CsvData.Gacha]; // csv데이터로 가챠리스트 가져오기
+        for(int i = 0; i < gachaList.Count; i++)
+        {
+
+        }
         // TODO : csv 파일에서 GachaID의 개수를 파악하여 필요한 총 List 생성
+
     }
 }
