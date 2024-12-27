9d579a54808d5326e253300dae0cd8e489dc571e
whs1997 <whs1997@naver.com>
2024-12-27 21:27:44 +0900

    [FEAT] 방치형 시간 저장하기

--- Assets/Programing/WHS/Scripts/IdleReward/RoomPanel.cs
+++ Assets/Programing/WHS/Scripts/IdleReward/RoomPanel.cs
@@ -2,8 +2,18 @@ using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class RoomPanel : MonoBehaviour
 {
+    [SerializeField] private IdleReward idleReward;
 
+    private void Awake()
+    {
+    }
+
+    private void OnDisable()
+    {
+        idleReward.SaveExitTime();
+    }
 }
