3cd46c8ccef4449a7940b7091066996c5953ffaf
jongddak <hfsmsj316@gmail.com>
2025-01-07 21:49:00 +0900

    fix gachaCheck 코드 주석 변경

--- Assets/Programing/YJE/Script/GachaCheck.cs
+++ Assets/Programing/YJE/Script/GachaCheck.cs
@@ -36,45 +36,45 @@ public class GachaCheck : MonoBehaviour
                     result = playerData.Items[(int)E_Item.Coin] + amount;
                     playerData.SetItem((int)E_Item.Coin, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/0");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/0");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/0");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/0");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "DinoBlood":
                     result = playerData.Items[(int)E_Item.DinoBlood] + amount;
                     playerData.SetItem((int)E_Item.DinoBlood, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/1");
+                     setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/1");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/1");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/1");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "BoneCrystal":
                     result = playerData.Items[(int)E_Item.BoneCrystal] + amount;
                     playerData.SetItem((int)E_Item.BoneCrystal, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/2");
+                     setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/2");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/2");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/2");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "DinoStone":
                     result = playerData.Items[(int)E_Item.DinoStone] + amount;
                     playerData.SetItem((int)E_Item.DinoStone, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/3");
+                     setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/3");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/3");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/3");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "Stone":
                     result = playerData.Items[(int)E_Item.Stone] + amount;
                     playerData.SetItem((int)E_Item.Stone, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/4");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/4");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/4");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/4");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 default:
@@ -89,45 +89,45 @@ public class GachaCheck : MonoBehaviour
                     result = playerData.Items[(int)E_Item.Coin] - amount;
                     playerData.SetItem((int)E_Item.Coin, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/0");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/0");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/0");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/0");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "DinoBlood":
                     result = playerData.Items[(int)E_Item.DinoBlood] - amount;
                     playerData.SetItem((int)E_Item.DinoBlood, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/1");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/1");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/1");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/1");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "BoneCrystal":
                     result = playerData.Items[(int)E_Item.BoneCrystal] - amount;
                     playerData.SetItem((int)E_Item.BoneCrystal, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/2");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/2");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/2");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/2");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "DinoStone":
                     result = playerData.Items[(int)E_Item.DinoStone] - amount;
                     playerData.SetItem((int)E_Item.DinoStone, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/3");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/3");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/3");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/3");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 case "Stone":
                     result = playerData.Items[(int)E_Item.Stone] - amount;
                     playerData.SetItem((int)E_Item.Stone, result);
                     // 실제 빌드 시 사용 - UserId불러오기
-                    // setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/4");
+                    setItemRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_items/4");
                     // Test용
-                    setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/4");
+                    //setItemRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_items/4");
                     setItemRoot.SetValueAsync(result); // firebase 값 변경
                     break;
                 default:
@@ -174,9 +174,9 @@ public class GachaCheck : MonoBehaviour
                     newUnit.UnitLevel = 1;
                     playerData.UnitDatas.Add(newUnit);
                     // 실제 빌드 시 사용 - UserId불러오기 
-                    // DatabaseReference unitRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_unitDatas");
+                    DatabaseReference unitRoot = root.Child(BackendManager.Auth.CurrentUser.UserId).Child("_unitDatas");
                     // Test 용
-                    DatabaseReference unitRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_unitDatas");
+                    //DatabaseReference unitRoot = root.Child("CHSmbrwghYNzZb7AIkdLRtvpHaW2").Child("_unitDatas");
 
                     // 모든 playerData.UnitDatas의 정보를 DB서버에 갱신
                     for (int num = 0; num < playerData.UnitDatas.Count; num++)
